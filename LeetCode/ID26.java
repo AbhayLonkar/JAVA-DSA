bca33bf03434296c84fe0c7c0506f0296f2c8dd1
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-05-26 17:25:51 +0530

    Day 7 - keeping the progress going!

---
+++ LeetCode/ID26.java
@@ -0,0 +1,21 @@
+// 26. Remove Duplicates from Sorted Array
+
+package LeetCode;
+
+public class ID26 {
+  public static void main(String[] args) {
+    int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+    System.out.println(removeDuplicates(arr));
+  }
+
+  static int removeDuplicates(int[] arr) {
+    int k = 0;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[k] != arr[i]) {
+        k++;
+        arr[k] = arr[i];
+      }
+    }
+    return k + 1;
+  }
+}
