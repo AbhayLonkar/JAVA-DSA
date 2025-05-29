b5d7a99c6573e7aa3c59599aa3322d55499ef3a0
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-05-29 13:19:29 +0530

    Day 9

--- LeetCode/ID560.java
+++ LeetCode/ID560.java
@@ -1,5 +1,7 @@
 // 560. Subarray Sum Equals K
 
+import java.util.*;
+
 public class ID560 {
   /*
    * Solved using Brute Force
@@ -10,10 +12,11 @@ public class ID560 {
   public static void main(String args[]) {
     int[] nums = { 1, 2, 3 };
     int k = 3;
-    System.out.println(subarraySum(nums, k));
+    System.out.println(subarraySum2(nums, k));
   }
 
-  static int subarraySum(int[] nums, int k) {
+  // Brute Force
+  static int subarraySum1(int[] nums, int k) {
     int count = 0;
     for (int i = 0; i < nums.length; i++) {
       int sum = 0;
@@ -23,6 +26,21 @@ public class ID560 {
           count++;
         }
       }
+    }
+    return count;
+  }
+
+  // Optimized
+  static int subarraySum2(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+    int count = 0, prefixSum = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      prefixSum += nums[i];
+      int remove = prefixSum - k;
+      count += map.getOrDefault(remove, 0);
+      map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
     }
     return count;
   }
